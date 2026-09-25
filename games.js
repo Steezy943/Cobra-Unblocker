001c3f6c1e0278d23222074cb693733f621a28af
Steezy <steezywuzhere@gmail.com>
2026-09-25 09:46:06 -0400

    Create games.js

---
+++ games.js
@@ -0,0 +1,118 @@
+// 🎮 COBRA NETWORK DYNAMIC LINK CONFIGURATION CATALOGUE
+// Add, remove, or modify items here without touching main script functionality.
+export const gamesList = [
+    { 
+        title: "Untitled Goose Game", 
+        category: "Casual", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/Untitled Goose Game.jpg" 
+    },
+    { 
+        title: "Hollow Knight", 
+        category: "Action", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/Hollow Knight.webp" 
+    },
+    { 
+        title: "Half Life", 
+        category: "Action", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/Half Life 1.jpg" 
+    },
+    { 
+        title: "Karlson", 
+        category: "Action", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/Karlson.png" 
+    },
+    { 
+        title: "PEAK", 
+        category: "Adventure", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/Peak.jpg"
+    },
+    { 
+        title: "How To Fish", 
+        category: "Adventure/Action", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/HowToFish.jpg" 
+    },
+    { 
+        title: "Slendy Tubbies", 
+        category: "Adventure/Horror", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/slendytubbies.jpg" 
+    },
+    { 
+        title: "Cat Goes Fishing", 
+        category: "Adventure/Peacful", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/Cat Goes Fishing.jpg" 
+    },
+    { 
+        title: "Geometry Dash [Beta]", 
+        category: "Adventure/Intense", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/Geometry Dash.png" 
+    },
+    { 
+        title: "GD FULL V1", 
+        category: "Rythm/Platformer", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/Geometry Dash V1.png" 
+    },
+    { 
+        title: "Ultrakill", 
+        category: "Action/Shooter/Platformer", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/Ultrakill.jpg" 
+    },
+    { 
+        title: "Hollow Knight Silksong", 
+        category: "Action/Platformer", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/HollowKnightSilk.jpg" 
+    },
+    { 
+        title: "Clover Pit", 
+        category: "Intense/LuckBased", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/CloverPit.jpg" 
+    },
+    { 
+        title: "Fears to Fathom HA", 
+        category: "Intense/Horror", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/F2FHA.jpg" 
+    },
+    { 
+        title: "TABS", 
+        category: "Intense/War", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/TABS.jpg" 
+    },
+    { 
+        title: "Ravenfield", 
+        category: "Intense/War", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/RavenField.png" 
+    },
+    { 
+        title: "20 Minutes Till Dawn", 
+        category: "Survival/Rougelike/Intense", 
+        gameUrl: "https://github.io",
+        thumbUrl: "Assets/Thumbnails/20minstd.png" 
+    },
+    { 
+        title: "A Game About Climbing", 
+        category: "Rage/Adventure", 
+        gameUrl: "https://steezy943.github.io/adgac-webport/",
+        thumbUrl: "Assets/Thumbnails/adgac.png" 
+    },
+    { 
+        title: "CounterStrike:Source 2d", 
+        category: "Shooting/Intense", 
+        gameUrl: "https://frankplus.github.io/",
+        thumbUrl: "Assets/Thumbnails/CSS2D.png" 
+    }
+];
